f36993a7864b86f4e6c0f970e87e26247e2eb12b
Mahin Sugrim <mahinsugrim@gmail.com>
2020-03-04 00:14:07 -0400

    Answer for Q4

--- population_queries.sql
+++ population_queries.sql
@@ -1,3 +1,10 @@
 -- Q3 - What years are covered by the dataset? (you can manually count the number of years returned).
 SELECT DISTINCT year
 FROM population_years;
+
+-- Q4 - What is the largest population size for Gabon in this dataset?
+SELECT *
+FROM population_years
+WHERE country = 'Gabon'
+ORDER BY population DESC
+LIMIT 1;
